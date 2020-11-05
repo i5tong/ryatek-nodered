2364c7b3409129f9bab4b02774bdf174a20e0cd3
zhangliang <zhangl1@suirui.com>
2020-11-05 12:02:13 +0800

    package

---
+++ nodes/if.js
@@ -0,0 +1,24 @@
+module.exports = function (RED) {
+    class IfNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            // 设定状态
+            this.status({fill: 'grey', shape: 'ring', text: 'false'});
+            this.on('input', msg => {
+                if (msg.topic === config.topic) {
+                    this.state = Boolean(msg.payload);
+                    if (this.state) {
+                        this.status({fill: 'blue', shape: 'dot', text: 'true'});
+                    } else {
+                        this.status({fill: 'grey', shape: 'ring', text: 'false'});
+                    }
+                } else if (this.state) {
+                    this.send([msg, null]);
+                } else {
+                    this.send([null, msg]);
+                }
+            });
+        }
+    }
+    RED.nodes.registerType('if', IfNode);
+};
